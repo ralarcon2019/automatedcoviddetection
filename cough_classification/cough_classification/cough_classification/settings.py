1a8197a2050f49fa49ca3791c31de430e3241ede
ralarcon2019 <76644162+ralarcon2019@users.noreply.github.com>
2024-10-24 22:32:12 -0400

    initial commit

--- cough_classification/cough_classification/cough_classification/settings.py
+++ cough_classification/cough_classification/cough_classification/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-x3!p+09)do=vu#py97vx_cm6!v!u*q6@87*=z2@^zc8to4=#b#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
